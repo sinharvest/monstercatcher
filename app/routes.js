a7f4aa2362f1d5bcc25101b4679be05f011282ac
sinharvest <myhandle@houstonhunt.com>
2014-03-20 11:04:10 -0400

    idk

--- app/routes.js
+++ app/routes.js
@@ -1,4 +1,9 @@
 var User = require('./models/user');
+var request = require('request');
+var moment = require('moment');
+var configAuth = require('../config/auth.js');
+var Fitbit = require('fitbit');
+
 //app/routes.js
 module.exports = function(app, passport, db){
 
@@ -14,17 +19,57 @@ module.exports = function(app, passport, db){
 
 //omitted signup part of tutorial
 
-//profile section
-//show stats
+//===============profile section================================
   app.get('/profile',isLoggedIn, function(req,res){
-	console.log('req is ::::: '+req.name);
-	console.log('req.user ::::: '+req.user);
+//	console.log('req is ::::: '+req.name);
+//	console.log('req.user ::::: '+req.user);
         var query = User.where({id:req.user});
 	query.findOne(function(err, kitten){console.log('kitty ='+kitten);
-	
-	console.log('SWAG = '+query);//findOne({ '': 'Ghost' },
-    res.render('profile.ejs', {
-	user:kitten//pass user to template
+	var info = kitten;
+//        console.log("setGoal is :  "+kitten.setGoal);	
+//	console.log('SWAG = '+query);//findOne({ '': 'Ghost' },
+        
+
+        //use request for fitbit api to get activities information
+//	var options = {
+  //        url: '/1/user/-/activities/date/'+moment().format(YYYY-MM-DD)+'.json',
+//	  headers: {
+//		    'api.fitbit.com',
+//		    'OAuth oauth_consumer_key='+fitbitAuth.consumerKey,
+//		    kitten.accessToken,
+//		    "HMAC-SHA1",
+//		    "1.0"
+//	           }
+//	};
+//
+///	function callback(error, response, body) {
+///		    if (!error && response.statusCode == 200) {
+//			            var info = JSON.parse(body);
+//				            console.log(info.stargazers_count + " Stars");
+//					            console.log(info.forks_count + " Forks");
+//						        }
+//	}
+//
+//	request(options, callback);
+        client = new Fitbit(
+		configAuth.fitbitAuth.consumerKey,
+		configAuth.fitbitAuth.consumerSecret,
+		{
+		accessToken: kitten.accessToken,
+		accessTokenSecret: kitten.accessSecret,
+		unitMeasure: 'en_GB'
+		}
+	);
+	//get todays activities
+	client.getActivities(function(err,activities){
+		if(err){console.log('FAILLLL'); return;}
+		console.log('success! steps are '+activities.steps());
+		console.log('success! calories are '+activities.activityCalories());
+	});
+
+
+        res.render('profile.ejs', {
+	  user:kitten//pass user to template
 	});
     });
   });
@@ -56,7 +101,6 @@ module.exports = function(app, passport, db){
 
 //route middleware to make sure a user is logged in
   function isLoggedIn(req, res, next){
-    console.log("what the fuck is req auth? -->"+req);
     if(req.isAuthenticated())
       return next();
     res.redirect('/');
